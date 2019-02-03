686612a1595564283496e0670a95d3459333fc66
miRoox <miroox@outlook.com>
2019-02-03 20:39:34 +0800

    package prototype

---
+++ ComputationalOptics/PacletInfo.m
@@ -0,0 +1,13 @@
+
+Paclet[
+  Name->"ComputationalOptics",
+  Version->"0.0.0",
+  Creator->"miRoox <miRoox@outlook.com>",
+  Description->"Computational optics toolkit.",
+  Extensions->{
+    {
+      "Kernel",
+      "Context"->{"ComputationalOptics`"}
+    }
+  }
+]
