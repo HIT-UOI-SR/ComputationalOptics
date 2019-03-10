5ba2f0b45f98883496946030869188d0330cc261
miRoox <miroox@outlook.com>
2019-03-10 21:34:05 +0800

    D-FFT Fresnel diffraction

--- ComputationalOptics/Kernel/Propagation.m
+++ ComputationalOptics/Kernel/Propagation.m
@@ -51,3 +51,25 @@ PropagationFresnel1[input_?MatrixQ,lambda_?Positive,d_?realNumberQ]:=
     opticalFourier[input*q]q*Exp[2Pi I d/lambda]/(I lambda d)
   ]
 call_PropagationFresnel1:=(Message[PropagationFresnel1::invarg,HoldForm@call];$Failed)
+
+
+PackageExport["PropagationFresnel2"]
+SetUsage[PropagationFresnel2,
+  "PropagationFresnel2[input$, \[Lambda]$, d$] calculate propagation of the input$ field with the distance d$ and the wavelength \[Lambda]$ based on the Fresnel diffraction in the form of convolution."
+]
+PropagationFresnel2::cond="Warning: The distance `1` may be too small to satisfy the Fresnel approximation."
+PropagationFresnel2::invarg="Call `1` with the invalid argument."
+tfFresnel2:=Memoized@
+  Compile[{{lambda,_Real}, {d,_Real}, {w,_Integer}, {l,_Integer}},
+    Outer[Exp[-Pi I d/lambda (#1^2+#2^2)]&,
+      Range[w]/w-1/2.,
+      Range[l]/l-1/2.
+    ]Exp[2Pi I d/lambda]
+  ]
+checkFresnel2Cond[lambda_,d_,{w_,l_}]:=
+  If[d^3<5/(8lambda)*Sqrt[w^2+l^2],Message[PropagationFresnel2::cond,d]]
+PropagationFresnel2[input_?MatrixQ,lambda_?Positive,d_?realNumberQ]:= (
+  checkFresnel2Cond[lambda,d,Dimensions[input]];
+  opticalInverseFourier[opticalFourier[input]tfFresnel2[lambda,d,Sequence@@Dimensions[input]]]
+)
+call_PropagationFresnel2:=(Message[PropagationFresnel2::invarg,HoldForm@call];$Failed)
