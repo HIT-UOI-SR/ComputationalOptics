d019f41846682a1f293d1e96dd4118bf668aca36
miRoox <miroox@outlook.com>
2019-04-25 22:45:48 +0800

    reform: Propagation
    * argument can be quantity with unit
    * implement based on the direct scale

--- ComputationalOptics/Kernel/AllCommon.m
+++ ComputationalOptics/Kernel/AllCommon.m
@@ -7,5 +7,8 @@ PackageImport["GeneralUtilities`"]
 PackageScope["objectOptionValue"]
 objectOptionValue[obj_,opt_]:=Lookup[Options@obj,opt]
 
-PackageScope["realNumberQ"]
-realNumberQ[expr_]:=MatchQ[Quiet@N@expr,_Real]
+PackageScopr["nVal"]
+nVal[nums_]:=N@QuantityMagnitude@CommonUnits[nums]
+
+PackageScope["realQ"]
+realQ[expr_]:=NonNegative[expr]||NonPositive[expr]
--- ComputationalOptics/Kernel/Propagation.m
+++ ComputationalOptics/Kernel/Propagation.m
@@ -6,102 +6,107 @@ PackageImport["GeneralUtilities`"]
 
 PackageScope["opticalFourier"]
 PackageScope["opticalInverseFourier"]
-PackageScope["realNumberQ"]
+PackageScope["realQ"]
+PackageScope["nVal"]
 
 
 PackageExport["PropagationAS"]
 SetAttributes[PropagationAS,ReadProtected]
 SetUsage[PropagationAS,
-  "PropagationAS[input$, wavelength$, distance$]\
+  "PropagationAS[input$, wavelength$, distance$, size$]\
   calculates the propagation light field based on the angular spectrum."
 ]
 PropagationAS::invarg="Call `1` with the invalid argument."
-tfAS:=Memoized@FunctionCompile@Function[
-  {Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"UnsignedMachineInteger"],Typed[l,"UnsignedMachineInteger"]},
+tfAS:=Memoized@FunctionCompile@Function[{
+    Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"Real64"],Typed[l,"Real64"],
+    Typed[nw,"UnsignedMachineInteger"],Typed[nl,"UnsignedMachineInteger"]},
   Table[Exp[2Pi I d Sqrt@Compile`Cast[1/lambda^2-x^2-y^2,"ComplexReal64"]],
-    {x,1.0/w-1/2.,1/2.,1.0/w},
-    {y,1.0/l-1/2.,1/2.,1.0/l}
+    {x,1.0/w-nw/(2w),nw/(2w),1.0/w},
+    {y,1.0/l-nl/(2l),nl/(2l),1.0/l}
   ]
 ]
-PropagationAS[input_?MatrixQ,lambda_?Positive,d_?realNumberQ]:=
-  opticalInverseFourier[opticalFourier[input]tfAS[lambda,d,Sequence@@Dimensions[input]]]
+iPropagationAS[input_,{lambda_,d_,w_,l_},{nw_,nl_}]:=
+  opticalInverseFourier[opticalFourier[input]tfAS[lambda,d,w,l,nw,nl]]
+PropagationAS[input_?MatrixQ,lambda_?Positive,d_?realQ]:=Scope[
+  {w,l}=Dimensions[input];
+  iPropagationAS[input,nVal@{lambda,d,w,l},{w,l}]
+]
+PropagationAS[input_?MatrixQ,lambda_?Positive,d_?realQ,sz_?Positive]:=
+  iPropagationAS[input,nVal@{lambda,d,sz,sz},Dimensions[input]]
+PropagationAS[input_?MatrixQ,lambda_?Positive,d_?realQ,{w_?Positive,l_?Positive}]:=
+  iPropagationAS[input,nVal@{lambda,d,w,l},Dimensions[input]]
 call_PropagationAS:=(Message[PropagationAS::invarg,HoldForm@call];$Failed)
 
 
 PackageExport["PropagationFresnel1"]
 SetAttributes[PropagationFresnel1,ReadProtected]
 SetUsage[PropagationFresnel1,
-  "PropagationFresnel1[input$, wavelength$, distance$]\
+  "PropagationFresnel1[input$, wavelength$, distance$, size$]\
    calculates the propagation light field based on the Fresnel diffraction in the form of Fourier transform."
 ]
 PropagationFresnel1::cond="Warning: The distance `1` may be too small to satisfy the Fresnel approximation."
 PropagationFresnel1::invarg="Call `1` with the invalid argument."
-fresnel1Q1:=Memoized@FunctionCompile@Function[
-  {Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"UnsignedMachineInteger"],Typed[l,"UnsignedMachineInteger"]},
-  Table[Exp[I Pi/(lambda d) (x^2+y^2)],
-    {x,1-w/2.,w/2.,1.},
-    {y,1-l/2.,l/2.,1.}
-  ]
+$FresnelCondChecked
+checkFresnelCond[f:(PropagationFresnel1|PropagationFresnel2),lambda_,d_,w_,l_]/;$FresnelCondChecked:=True
+checkFresnelCond[f:(PropagationFresnel1|PropagationFresnel2),lambda_,d_,w_,l_]:=Scope[
+  If[d^3<1/(16lambda)*(w^2+l^2)^2,
+    Message[f::cond,d]
+  ];
+  True
 ]
-fresnel1Q2:=Memoized@FunctionCompile@Function[
-  {Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"UnsignedMachineInteger"],Typed[l,"UnsignedMachineInteger"]},
-  Table[Exp[I Pi d/lambda (x^2+y^2)]*Exp[2Pi I d/lambda]/(I lambda d),
-    {x,1.0/w-1/2.,1/2.,1.0/w},
-    {y,1.0/l-1/2.,1/2.,1.0/l}
-  ]
-]
-checkFresnel1Cond[lambda_,d_,{w_,l_}]:=
-  If[d^3<5/(8lambda)*Sqrt[w^2+l^2],Message[PropagationFresnel1::cond,d]]
-PropagationFresnel1[input_?MatrixQ,lambda_?Positive,d_?realNumberQ]:=
-  Scope[
-    checkFresnel1Cond[lambda,d,Dimensions[input]];
-    q1=fresnel1Q1[lambda,d,Sequence@@Dimensions[input]];
-    q2=fresnel1Q2[lambda,d,Sequence@@Dimensions[input]];
-    opticalFourier[input*q1]q2
-  ]
-call_PropagationFresnel1:=(Message[PropagationFresnel1::invarg,HoldForm@call];$Failed)
-
-PackageExport["PropagationFresnel1X"]
-fresnel1QX:=Memoized@FunctionCompile@Function[
-  {Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"Real64"],Typed[l,"Real64"],
+(_checkFresnelCond)=False
+fresnel1Q:=Memoized@FunctionCompile@Function[{
+    Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"Real64"],Typed[l,"Real64"],
     Typed[nw,"UnsignedMachineInteger"],Typed[nl,"UnsignedMachineInteger"]},
   Table[Exp[I Pi/(lambda d) (x^2+y^2)],
     {x,w/nw-w/2.,w/2.,w/nw},
     {y,l/nl-l/2.,l/2.,l/nl}
   ]
 ]
-PropagationFresnel1X[input_?MatrixQ,lambda_?Positive,d_?realNumberQ,sz_]:=PropagationFresnel1X[input,lambda,d,{sz,sz}]
-PropagationFresnel1X[input_?MatrixQ,lambda_?Positive,d_?realNumberQ,{w_,l_}]:=
-  Scope[
-    checkFresnel1Cond[lambda,d,Dimensions[input]];
-    {nw,nl}=Dimensions[input];
-    {dw,dl}={w,l}/{nw,nl};
-    q1=fresnel1QX[lambda,d,w,l,nw,nl];
-    {wo,lo}={nw,nl}*lambda*d/{w,l};
-    q2=fresnel1QX[lambda,d,wo,lo,nw,nl]*dw*dl*Exp[2Pi I d/lambda]/(I lambda d);
-    opticalFourier[input*q1]q2
-  ]
+iPropagationFresnel1[input_,{lambda_,d_,w_,l_},{nw_,nl_}]:=Scope[
+  q1=fresnel1Q[lambda,d,w,l,nw,nl];
+  {dw,dl}={w,l}/{nw,nl};
+  {w2,l2}=lambda d/{dw,dl};
+  q2=fresnel1Q[lambda,d,w2,l2,nw,nl]Exp[2Pi I d/lambda]/(I lambda d);
+  opticalFourier[input*q1]*dw*dl*q2
+]
+PropagationFresnel1[input_?MatrixQ,lambda_?Positive,d_?realQ]:=With[
+  {sz=Dimensions[input]},
+  {w=sz[[1]],l=sz[[-1]]},
+  iPropagationFresnel1[input,nVal@{lambda,d,w,l},{w,l}]/;checkFresnelCond[PropagationFresnel1,lambda,d,w,l]
+]
+PropagationFresnel1[input_?MatrixQ,lambda_?Positive,d_?realQ,sz_?Positive]/;checkFresnelCond[PropagationFresnel1,lambda,d,sz,sz]:=
+  iPropagationFresnel1[input,nVal@{lambda,d,sz,sz},Dimensions[input]]
+PropagationFresnel1[input_?MatrixQ,lambda_?Positive,d_?realQ,{w_?Positive,l_?Positive}]/;checkFresnelCond[PropagationFresnel1,lambda,d,w,l]:=
+  iPropagationFresnel1[input,nVal@{lambda,d,w,l},Dimensions[input]]
+call_PropagationFresnel1:=(Message[PropagationFresnel1::invarg,HoldForm@call];$Failed)
 
 
 PackageExport["PropagationFresnel2"]
 SetAttributes[PropagationFresnel2,ReadProtected]
 SetUsage[PropagationFresnel2,
-  "PropagationFresnel2[input$, wavelength$, distance$]\
+  "PropagationFresnel2[input$, wavelength$, distance$, size$]\
   calculates the propagation light field based on the Fresnel diffraction in the form of convolution."
 ]
 PropagationFresnel2::cond="Warning: The distance `1` may be too small to satisfy the Fresnel approximation."
 PropagationFresnel2::invarg="Call `1` with the invalid argument."
-tfFresnel2:=Memoized@FunctionCompile@Function[
-  {Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"UnsignedMachineInteger"],Typed[l,"UnsignedMachineInteger"]},
-  Table[Exp[-Pi I d/lambda  (x^2+y^2)]*Exp[2Pi I d/lambda],
-    {x,1.0/w-1/2.,1/2.,1.0/w},
-    {y,1.0/l-1/2.,1/2.,1.0/l}
+tfFresnel2:=Memoized@FunctionCompile@Function[{
+    Typed[lambda,"Real64"],Typed[d,"Real64"],Typed[w,"Real64"],Typed[l,"Real64"],
+    Typed[nw,"UnsignedMachineInteger"],Typed[nl,"UnsignedMachineInteger"]},
+  Table[Exp[-Pi I lambda d (x^2+y^2)]*Exp[2Pi I d/lambda],
+    {x,1.0/w-nw/(2w),nw/(2w),1.0/w},
+    {y,1.0/l-nl/(2l),nl/(2l),1.0/l}
   ]
 ]
-checkFresnel2Cond[lambda_,d_,{w_,l_}]:=
-  If[d^3<5/(8lambda)*Sqrt[w^2+l^2],Message[PropagationFresnel2::cond,d]]
-PropagationFresnel2[input_?MatrixQ,lambda_?Positive,d_?realNumberQ]:= (
-  checkFresnel2Cond[lambda,d,Dimensions[input]];
-  opticalInverseFourier[opticalFourier[input]tfFresnel2[lambda,d,Sequence@@Dimensions[input]]]
-)
+iPropagationFresnel2[input_,{lambda_,d_,w_,l_},{nw_,nl_}]:=
+  opticalInverseFourier[opticalFourier[input]tfFresnel2[lambda,d,w,l,nw,nl]]
+PropagationFresnel2[input_?MatrixQ,lambda_?Positive,d_?realQ]:=With[
+  {sz=Dimensions[input]},
+  {w=sz[[1]],l=sz[[-1]]},
+  iPropagationFresnel2[input,nVal@{lambda,d,w,l},{w,l}]/;checkFresnelCond[PropagationFresnel2,lambda,d,w,l]
+]
+PropagationFresnel2[input_?MatrixQ,lambda_?Positive,d_?realQ,sz_?Positive]/;checkFresnelCond[PropagationFresnel2,lambda,d,sz,sz]:=
+  iPropagationFresnel2[input,nVal@{lambda,d,sz,sz},Dimensions[input]]
+PropagationFresnel2[input_?MatrixQ,lambda_?Positive,d_?realQ,{w_?Positive,l_?Positive}]/;checkFresnelCond[PropagationFresnel2,lambda,d,w,l]:=
+  iPropagationFresnel2[input,nVal@{lambda,d,w,l},Dimensions[input]]
 call_PropagationFresnel2:=(Message[PropagationFresnel2::invarg,HoldForm@call];$Failed)
