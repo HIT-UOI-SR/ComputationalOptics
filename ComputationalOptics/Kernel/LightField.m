dda467dbb17788874a951b917a7b7d55cf7f3112
miRoox <miroox@outlook.com>
2019-04-27 17:04:55 +0800

    prototype of LightField object

---
+++ ComputationalOptics/Kernel/LightField.m
@@ -0,0 +1,17 @@
+Package["ComputationalOptics`"]
+
+
+PackageImport["GeneralUtilities`"]
+
+
+PackageExport["LightField"]
+SetUsage[LightField,
+  "LightField[type$, data$] represents an object containing the light field information."
+]
+
+
+PackageExport["$LightFieldTypes"]
+SetUsage[$LightFieldTypes,
+  "$LightFieldTypes gives a list of light field types that can be used."
+]
+
