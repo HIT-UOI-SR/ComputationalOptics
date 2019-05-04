a04ec46a2cd095d1c7e8c59108f3c37ddb906156
miRoox <miroox@outlook.com>
2019-05-04 20:55:05 +0800

    better Mathematica-style API design

--- ComputationalOptics/Kernel/LightField.m
+++ ComputationalOptics/Kernel/LightField.m
@@ -6,12 +6,24 @@ PackageImport["GeneralUtilities`"]
 
 PackageExport["LightField"]
 SetUsage[LightField,
-  "LightField[type$, data$] represents an object containing the light field information."
+  "LightField[type$, data$] represents an object containing the light field information.",
+  "LightField[\"Types\"] gives a list of the vaild light field types.",
+  "LightField[type$, \"Properties\"] gives a list of the valid properties for the type$."
 ]
 
-
-PackageExport["$LightFieldTypes"]
-SetUsage[$LightFieldTypes,
-  "$LightFieldTypes gives a list of light field types that can be used."
+$types=<|
+  "PlaneWave"->{"Data","Wavelength","PhysicalSize"}
+|>
+LightField["Types"]=Keys[$types]
+LightField[type_String,"Properties"]:=With[
+  {prop=$types[type]},
+  If[MissingQ[prop],
+    Missing["NotAvailable",type],
+    prop
+  ]
 ]
 
+PackageExport["LightFieldQ"]
+SetUsage[LightFieldQ,
+  "LightFieldQ[expr$] yields True if expr$ is a valid LightField object."
+]
