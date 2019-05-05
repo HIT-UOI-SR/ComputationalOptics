3c1340c36e45ab9ccbc2afe3c2e2a685bfc0a6f5
miRoox <miroox@outlook.com>
2019-05-05 11:00:30 +0800

    getter & setter

--- ComputationalOptics/Kernel/LightField.m
+++ ComputationalOptics/Kernel/LightField.m
@@ -20,6 +20,9 @@ SetUsage[LightFieldQ,
 
 
 LightField::notype="`1` is not available type."
+LightField::setptv="`1` is not valid data in part assignment."
+LightField::setptp="`1` is not valid property in part assignment."
+LightField::setptvp="`1` is not valid value for `2` in part assignment."
 
 
 $types=<|
@@ -34,6 +37,42 @@ LightField[type_String,"Properties"]:=With[
     prop
   ]
 ]
+
+
+getType[obj_LightField]:=obj[[1]]
+getData[obj_LightField,All]:=obj[[2]]
+getData[obj_LightField,prop_]:=obj[[2,Key[prop]]]
+SetAttributes[setData,HoldFirst]
+setData[sym_Symbol,All,val_]:=Block[
+  {tmp=ReplacePart[sym,2->val]},
+  If[LightFieldQ[tmp],
+    sym=tmp;val,
+    Message[LightField::setptv,val];
+    $Failed
+  ]
+]
+setData[sym_Symbol,prop_,val_]:=Block[
+  {tmp=sym},
+  If[KeyExistsQ[tmp[[2]],prop],
+    tmp=ReplacePart[tmp,{2,Key[prop]}->val];
+    If[LightFieldQ[tmp],
+      sym=tmp;val,
+      Message[LightField::setptvp,val,prop];
+      $Failed
+    ],
+    Message[LightField::setptp,prop];
+    $Failed
+  ]
+]
+(obj_LightField?LightFieldQ)["Type"]:=getType[obj]
+(obj_LightField?LightFieldQ)[prop_]:=getData[obj,prop]
+SetAttributes[mutationHandler,HoldAllComplete]
+mutationHandler[Set[(sym_Symbol?LightFieldQ)[prop_],val_]]:=With[
+  {result=setData[sym,prop,val]},
+  result/;!FailureQ[result]
+]
+Language`SetMutationHandler[LightField,mutationHandler]
+
 
 HoldPattern@LightFieldQ[
   LightField["PlaneWave",
